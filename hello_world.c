9b8f9567e5ac4ff664db7284efd5526a09df8972
gabriel schiavo <gabriel@lewagon.org>
2019-08-17 16:43:52 -0300

    add another prints

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+ 	int a, b;
+ 	float salary = 56.23;
+  char letter = 'Z';
+  a = 8;
+  b = 34;
+  int c = a+b;
+
+  printf("%d \n", c);
+  printf("%f \n", salary);
+  printf("%c \n", letter);
+  printf("Hello, World!\n");
+
+  return 0;
 }
--- sizeof.c
+++ sizeof.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  printf("int: %d \n", sizeof(int));
-  printf("float: %d \n", sizeof(float));
-  printf("double: %d \n", sizeof(double));
-  printf("char: %d \n", sizeof(char));
+  printf("int: %d \n", sizeof(int)); // => 4
+  printf("float: %d \n", sizeof(float)); // => 4
+  printf("double: %d \n", sizeof(double)); // => 8
+  printf("char: %d \n", sizeof(char)); // => 1
 
   return 0;
 }
