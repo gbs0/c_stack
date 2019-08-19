3fd4895f5ac734e97212e9fea7ed7f4e99132b5a
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 09:50:23 -0300

    add formatted options

--- outputs.c
+++ outputs.c
@@ -15,8 +15,21 @@ int foo() {
 
 	scanf("%2d %d %*f %5s", &x, &y, text);
 	/* input: 1234  5.7  elephant */
+	
 	printf("%d  %d  %s", x, y, text);
 	/* output: 12  34  eleph */ 	
+	
+	printf("Color: %s, Number: %d, float: %5.2f \n", "red", 42, 3.14159);
+	/* Color: red, Number: 42, float:  3.14  */
+
+	printf("Pi = %3.2f", 3.14159); 
+	/* Pi = 3.14 */
+
+	printf("Pi = %8.5f", 3.14159); 
+	/* Pi =   3.14159 */
+
+	printf("Pi = %-8.5f", 3.14159); 
+	/* Pi = 3.14159 */
 }
 
 
@@ -25,3 +38,11 @@ int bar()
 	printf("The tree has %d apples.\n", 22);
 	/* The tree has 22 apples. */
 }
+
+// Escape sequences:
+// \n new line
+// \t horizontal tab
+// \\ backslash
+// \b backspace
+// \' single quote
+// \" double quote
