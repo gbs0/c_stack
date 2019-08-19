2c34ecd73d3860cc5fee2c9d802aa987508f13bc
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 08:32:43 -0300

    add more notations

--- input.c
+++ input.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	char userInput = getchar();
+	char userInput = getchar();	// Gets user input
 
-	printf("%c\n", userInput);
+	printf("You entered: %c\n", userInput);
 	return 0;
+}
+
+int foo () {
+	char a[100]; // Array with 100 strings
+	gets(a); 	// Read input 
+
+  printf("You entered: %s", a);
+
+  return 0;
 }
