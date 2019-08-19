9783a879a6231c693699d707b8312696bfc75e48
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 08:27:00 -0300

    fix bug

---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Preprocessor directive
+#define PI 3.14
+
+int main() {
+	const double p = 3.14;
+  printf("%f", PI);
+  return 0;
+}
+
---
+++ input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	char userInput = getchar();
+
+	printf("%c\n", userInput);
+	return 0;
+}
