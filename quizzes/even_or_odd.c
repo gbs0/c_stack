e445655df9658496245ebdc7a156eed1bf60fd86
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 10:41:36 -0300

    add first challenge

--- quizzes/even_or_odd.c
+++ quizzes/even_or_odd.c
@@ -5,5 +5,16 @@
 
 int main()
 {
-
+	int i1 = 10;
+  int remainder, buzz, fizzbuzz;
+  float fizz;
+  
+  remainder = i1 % 2; 
+  fizz = i1 % 3; 
+  buzz = i1 % 5;
+    
+  printf("%d\n", remainder); 
+  printf("%f\n", fizz);
+  printf("%d\n", buzz);
+  
 }	
