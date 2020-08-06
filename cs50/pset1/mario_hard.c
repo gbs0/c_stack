9c8f08ea24da897e4789307c33ce680965ff9b8e
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-06 19:39:58 -0300

    worked

--- cs50/pset1/mario_hard.c
+++ cs50/pset1/mario_hard.c
@@ -5,36 +5,38 @@
 
 void func(int x)
 {
-   printf("%d",x);
+   
    switch(x)
    {
-      case 1: printf("       #\n");
-      case 2: printf("      ##\n");
+      case 1: printf("#\n"); break;
+      case 2: printf("#\n##\n"); break;
       //  case 3: printf("case 3.\n"); break;
-      case 3: printf("     ###\n");
-      case 4: printf("    ####\n");
-      case 5: printf("   #####\n");
-      case 6: printf("  ######\n");
-      case 7: printf(" #######\n");
-      case 8: printf("########\n");
+      case 3: printf("#\n##\n###\n"); break;
+      case 4: printf("#\n##\n###\n####\n");
+      case 5: printf("#\n##\n###\n####\n#####\n");
+      case 6: printf("#\n##\n###\n####\n#####\n######\n");
+      case 7: printf("#\n##\n###\n####\n#####\n######\n#######\n");
+      case 8: printf("#\n##\n###\n####\n#####\n######\n#######\n########\n");
       default: printf("\n");
    }
 }
 
 int main(void)
 {
-    bool isValid = false;
     int n_floor;
     
     do {
-		if (n_floor > 8) break;
-
 		printf("Height (1-8):\n");
 		scanf("%d", &n_floor);
-		
-		for(int i = 0; i < n_floor; ++i) {
-			printf("%s\n", "#");
-		}
+
+		if (n_floor > 8) break;
+
+		// for(int i = 0; i < n_floor; ++i) {
+		// 	printf("%s\n", "#");
+		// }
+
+		func(n_floor);
+
    	} while(n_floor >= 1 && n_floor <= 8); // Make sure the int is beetween 1..8 range
 }	
 
