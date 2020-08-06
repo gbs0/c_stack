8ba68138d030ea34219f63cfc913d29e3d11ec98
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-06 18:58:38 -0300

    half of the exercise is ready

--- cs50/pset1/mario_hard.c
+++ cs50/pset1/mario_hard.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include <stdbool.h>
 
+void func(int x)
+{
+   printf("%d",x);
+   switch(x)
+   {
+      case 1: printf("       #\n");
+      case 2: printf("      ##\n");
+      //  case 3: printf("case 3.\n"); break;
+      case 3: printf("     ###\n");
+      case 4: printf("    ####\n");
+      case 5: printf("   #####\n");
+      case 6: printf("  ######\n");
+      case 7: printf(" #######\n");
+      case 8: printf("########\n");
+      default: printf("\n");
+   }
+}
+
 int main(void)
 {
     bool isValid = false;
@@ -11,23 +29,10 @@ int main(void)
     do {
 		printf("Height (1-8):\n");
 		scanf("%d", &n_floor);
-		printf("\n got the number of floors: %d", n_floor);
+		printf("\n got the number of floors: %d \n", n_floor);
 
-   	} while(n_floor >= 1 && n_floor <= 8); // Make sure the int is beetween 1..8 range
+   	}while(n_floor >= 1 && n_floor <= 8); // Make sure the int is beetween 1..8 range
     
-    printf("hello, world\n");
-
-	switch(x)
-   	{
-      case 1: printf("       #");
-      case 2: printf("      ##");
-      //  case 3: printf("case 3.\n"); break;
-      case 3: printf("     ###");
-      case 4: printf("    ####");
-      case 5: printf("   #####");
-      case 6: printf("  ######");
-      case 7: printf(" #######");
-      case 8: printf("########");
-      default: printf("default.\n");
-   	}
+	for(int i = 0; i < n_floor; ++i) func(i);
 }	
+
