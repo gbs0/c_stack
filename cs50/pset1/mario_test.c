83895a335e864aa2d0df366105173d4f6dc59adc
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-06 15:57:54 -0300

    add initial code for the challenge

--- cs50/pset1/mario_test.c
+++ cs50/pset1/mario_test.c
@@ -24,28 +24,23 @@ void func(int x)
 int main(void)
 {
    int n_floor;
-
    n_floor = -1;
+   
    while (n_floor != 0)
    {
    	 printf("Height (1-8):\n");
    	 
-   	 // fgets(line, sizeof(line), stdin);
-   	 int scan_f_result = scanf("%d", &n_floor);
-   	 printf("\n got the scan: %d", scan_f_result);
+   	 scanf("%d", &n_floor);
+   	 
 	 printf("\n got the number of floors: %d", n_floor);
 
-   	 if ((scan_f_result == 0) | (scan_f_result == EOF))
+   	 if ((n_floor == 0) | (n_floor == EOF))
    	 {
-   		printf("got the number: %d", scan_f_result);
-   	 	
+
    	 	printf("\n *** You have to enter an integer! ***\n");
    	 	n_floor = -1;
    	 }
-   }
-   
-   // const int n_floor = scanf("%i", &input);
-   // printf("got the number: %d", n_floors);
 
-   for(int i = 0; i < n_floor; ++i) func(i);
+   	 for(int i = 0; i < n_floor; ++i) func(i);
+   }
 }
