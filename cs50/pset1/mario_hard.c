5e76742b9668ea4bb19a4286b9752dabc102442f
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-06 20:05:28 -0300

    add 4rd row

--- cs50/pset1/mario_hard.c
+++ cs50/pset1/mario_hard.c
@@ -11,7 +11,7 @@ void func(int x)
       case 1: printf("#  #\n"); break;
       case 2: printf(" #  #\n##  ##\n"); break;
       case 3: printf("  #  #\n ##  ##\n###  ###\n"); break;
-      case 4: printf("#\n##\n###\n####\n"); break;
+      case 4: printf("   #  #\n  ##  ##\n ###  ###\n####  ####\n"); break;
       case 5: printf("#\n##\n###\n####\n#####\n"); break ;
       case 6: printf("#\n##\n###\n####\n#####\n######\n"); break;
       case 7: printf("#\n##\n###\n####\n#####\n######\n#######\n"); break;
