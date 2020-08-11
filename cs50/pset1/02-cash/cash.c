afad6f2e2719b89232ceb6a977c692428d037636
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-11 20:51:10 -0300

    refactor calculation draw

--- cs50/pset1/02-cash/cash.c
+++ cs50/pset1/02-cash/cash.c
@@ -2,37 +2,42 @@
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
+#define COINS 4
+#define MAX 20
 
-int * calcDraw(int cents)
-{
+// void calcDraw(int cents)
+// {
+//     int amount_left = 0;
+//     int coins[COINS] = { 1, 5, 10, 25 };
+//     // int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+//     // int draw[MAX] = { 0 };
+
+//     int n = 4; // Size of coins array
+//     int i = n - 1;
+
+//     do {
+//         while(cents >= coins[i])
+//         {
+//             cents -= coins[i];
+//             draw[i] = coins[i];       
+//         }
+
+//         i -= 1;
     
-    static int draw[64];
-    int amount_left = 0;
-    int coins[] = { 1, 5, 10, 25 };
-    // int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
-    // int *draw[64];
+//     } while (i >= 0);
 
-    int n = 4; // Size of coins array
-    int i = n - 1;
-
-    do {
-        while(cents >= coins[i])
-        {
-            cents -= coins[i];
-            draw[i] = coins[i];       
-        }
-
-        i -= 1;
-    
-    } while (i >= 0);
-
-    
-    return draw;
-    printf("Populated array");
-}
+//     return draw;
+//     printf("Populated array");
+// }
 
 int main(void)
 {
+    int amount_left = 0;
+    int coins[] = { 1, 5, 10, 25 };
+    int draw[64];
+
+    int n = sizeof(coins);
+    int i = n - 1;
     float dollars;
     
     printf("Insert Money:\n");
@@ -44,6 +49,15 @@ int main(void)
     int cents = (int)round(dollars * 100); // Converting entered amount by 100, o factor with coin integer
     printf("%d\n", cents);
     
-    printf("%i\n", *calcDraw(cents));
+    do {
+        while(cents >= coins[i])
+        {
+            cents -= coins[i];
+            draw[i] = coins[i];
+        }
+        i -= 1;
+    } while ( i >= 0 );
+    // printf("%i\n", *calcDraw(cents));
+    printf("%i\n", *draw);
 }	
 
