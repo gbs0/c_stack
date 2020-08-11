35b2d6bf507bb9d64ee3e3a8fbc309da643b0737
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-11 18:38:42 -0300

    refactor c code

--- cs50/pset1/02-cash/cash.c
+++ cs50/pset1/02-cash/cash.c
@@ -3,14 +3,33 @@
 #include <string.h>
 #include <stdbool.h>
 
+int calcDraw(int cents)
+{
+    
+    int amount_left = 0;
+    int coins[] = [ 1, 5, 10, 25 ];
+    // int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+    int draw[] = [];
+
+    int n = 4; // Size of coins array
+    int i = n - 1;
+
+    do {
+        while(cents >= coins[i])
+        {
+            cents -= coins[i];
+            draw[i] = coins[i];       
+        }
+
+        i -= 1;
+    
+    } while (i >= 0);
+
+    return draw;
+}
+
 int main(void)
 {
-    int cents = 0; // Are *100 float value
-    int amount = 0;
-    
-    // int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
-    int coins[] = { 25, 10, 5, 1 };
-
     float dollars;
     
     printf("Insert Money:\n");
@@ -51,37 +70,10 @@ int main(void)
     //   }  
     // }
     
-    while(cents >= 25)
-    {
-      amount++;
-      cents -= coins[0];
-    }
-
-    while(cents >= 10)
-    {
-      amount++;
-      cents -= coins[1];
-    }
-
-    while(cents >= 5)
-    {
-      amount++;
-      cents -= coins[2];
-    }
-
-    while(cents >= 1)
-    {
-      amount++;
-      cents -= coins[3];
-    }
+    
     // for(int i = 0; i < dollars ; ++i) {
     //  printf("%s\n", "#");
     // }
     printf("%d\n", cents);
-
-    // do {
-  		
-
-   	// } while(dollars  >= 0 && dollars  <= 10); // Make sure the int is beetween 1..8 range
 }	
 
--- cs50/pset1/02-cash/cash.rb
+++ cs50/pset1/02-cash/cash.rb
@@ -1,32 +1,38 @@
 require 'pry'
 
-loop do
+def calcDraw(cents)
   amount_left = nil
   coins = [ 25, 10, 5 , 1 ]
   draw = []
 
+  n = coins.length
+  sortCoins = coins.reverse
+  i = n - 1
+  begin
+    
+    while(cents >= sortCoins[i])
+      cents -= sortCoins[i]
+      draw.push(sortCoins[i])
+    end
+
+    i -= 1
+
+    # i -= 1
+    # while(amount_left >= coins[1] )
+    #   amount_left -= coins[1]
+    #   draw += 1
+    # end
+  end while i >= 0
+
+  return draw
+end
+
+loop do
   print("Insert Money\n")
   print("$")
   
   input = gets.chomp.to_f
   cents = input * 100.round(2)
-  
-  n = coins.length
-
-  i = n - 1
-  while(i >= 0)
-    
-    while(cents >= coins[0])
-    	cents -= coins[0]
-    	draw.push(coins[0]) 
-    end
-
-    i -= 1
-    # while(amount_left >= coins[1] )
-    # 	amount_left -= coins[1]
-    # 	draw += 1
-    # end
-  end
-
-  p draw
+  p calcDraw(cents).length
 end
+
