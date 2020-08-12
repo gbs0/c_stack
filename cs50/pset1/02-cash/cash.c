229feb6e9cb12d1055c8ae8c3047bd075dbe9bd1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-12 13:57:59 -0300

    add new version for greedy algorithm

--- cs50/pset1/02-cash/cash.c
+++ cs50/pset1/02-cash/cash.c
@@ -1,69 +1,60 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+// C program to find minimum 
+// number of denominations 
+#include <stdio.h> 
 #include <math.h>
-#define COINS 4
-#define MAX 20
+#define COINS 4 
+#define NOTES 9
+#define MAX 20 
+  
+// All denominations of Indian Currency 
+int notes[NOTES] = { 1, 2, 5, 10, 20, 
+                     50, 100, 200, 2000 }; 
 
-void calcDraw(int cents)
-{
-    int amount_left = 0;
-    int coins[COINS] = { 1, 5, 10, 25 };
-    // int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
-    int draw[MAX] = { 0 };
-    int i, k = 0;
-    
-    // int n = 4; // Size of coins array
-    // int i = n - 1;
-
-    // do {
-    //     while(cents >= coins[i])
-    //     {
-    //         cents -= coins[i];
-    //         draw[i] = coins[i];       
-    //     }
-
-    //     i -= 1;
-    
-    // } while (i >= 0);
-
-    for (i = COINS - 1; i >= 0; i--)
-    {
-        while(cents >= coins[i]){
-            cents -= coins[i];
-            draw[k++] = coins[i];
-        }
-    }
+int coins[COINS] = { 1, 5, 10, 25 }; 
+  
+void findMin(int cost) 
+{ 
+    int coinList[MAX] = { 0 }; 
+    int i, k = 0; 
+  
+    for (i = COINS - 1; i >= 0; i--) { 
+        while (cost >= coins[i]) { 
+            cost -= coins[i]; 
+            // Add coin in the list 
+            coinList[k++] = coins[i]; 
+        } 
+    } 
+  
+    // for (i = 0; i < k; i++) { 
+    //     // Print 
+    //     printf("%d ", coinList[i]); 
+    // } 
+    printf("%d ", sizeof(coinList));
     return;
-    printf("Populated array");
-}
-
-int main(void)
-{
-    // int amount_left = 0;
-    // int coins[] = { 1, 5, 10, 25 };
-    // int draw[64];
-
-    // int n = sizeof(coins);
-    // int i = n - 1;
-
+} 
+  
+int main(void) 
+{ 
+    // input value
     float dollars;
-    
-    printf("Insert Money:\n");
-    printf("$");
 
-    scanf("%f", &dollars ); // Get value from terminal
-    // printf("%f\n", dollars);
-    
-    int cents = (int)round(dollars * 100); // Converting entered amount by 100, o factor with coin integer
-    
-    printf("Following is minimal number"
-           "of change for %d: ", 
-           cents); 
-    
-    calcDraw(cents);
-    
-    return 0;
-    // printf("%i\n", *calcDraw(cents));
-}	
+    do
+    {
+        printf("\nInsert Money:\n");
+        printf("$");
+        scanf("%f", &dollars ); // Get value from terminal
+        printf("%f\n", dollars);
+        
+        int cents = (int)round(dollars * 100);
 
+        printf("Following is minimal number "
+               "of change for %d: ", 
+               cents); 
+        findMin(cents); 
+        printf("Fina"
+               "of change for %d: ", 
+               cents); 
+        findMin(cents); 
+        //   return 0;
+    } while (dollars != 0.0);
+}
