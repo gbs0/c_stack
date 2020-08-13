55caa5ea216189d350bbc4907eb51acf7d8efc13
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-13 11:52:28 -0300

    init first versions for decypher

---
+++ cs50/pset2/caesar/caesar.rb
@@ -0,0 +1,10 @@
+# Caesar script for encrypt some string
+class Caesar
+  ALPHA = [*"A".."Z"] # Range for alphabet
+
+  def self.calCypher(words_range)
+  	key = 1
+  	cypher_index = (plain_index + key) % 26
+  end
+
+end
