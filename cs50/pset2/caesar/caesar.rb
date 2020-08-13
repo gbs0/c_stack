3e738e14e7b5b883d09ddf66cd4fafd3b72ac7d9
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-13 14:49:41 -0300

    add some args in initialize

--- cs50/pset2/caesar/caesar.rb
+++ cs50/pset2/caesar/caesar.rb
@@ -1,10 +1,22 @@
+require 'pry'
 # Caesar script for encrypt some string
 class Caesar
   ALPHA = [*"A".."Z"] # Range for alphabet
 
-  def self.calCypher(words_range)
-  	key = 1
-  	cypher_index = (plain_index + key) % 26
+  def initialize
+  	@args_key = ARGV[0];
+	@plaintext = ARGV[1];
+  end
+
+  def calcCypher
+  	# cypher_index = (plain_index + args_key) % 26 # Factor for get key cypher index
+  	puts @plaintext
+  	# puts ("cyphertext: #{cyphertext}")
   end
 
 end
+
+c = Caesar.new
+
+c.calcCypher()
+
