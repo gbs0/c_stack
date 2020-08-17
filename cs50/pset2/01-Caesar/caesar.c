317b36b639d34e1b177cde6ae49e5ece82791879
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-17 13:16:43 -0300

    working fine

--- cs50/pset2/01-Caesar/caesar.c
+++ cs50/pset2/01-Caesar/caesar.c
@@ -1,10 +1,7 @@
-#include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
-#include<regex.h>
-
 
 int main(int argc, char *argv[]) // get string for user
 {
@@ -21,22 +18,26 @@ int main(int argc, char *argv[]) // get string for user
 
 	printf("%s", "plaintext: "); // Ask for an plaintext
 	char plaintext;
-	scanf("%s", &plaintext );
+	scanf("%s", &plaintext);
 	printf("%s\n", &plaintext);
 	
-
-	// for (int i = 0; int len = strlen(plaintext); i < len; i++) // For loop for encipher each caracther
-	// {
-
-	// }
+	
+	for (int i = 0, len = strlen(&plaintext); i < len; i++) // For loop for encipher each caracther
+	{
+ 		char l = plaintext;
+ 		if(isalpha(l))
+ 		{
+ 			l++;
+ 			printf("%c\n", l + i);
+ 		}
+	}
+	
 	
 	// text[0] => 'T'
 	// text[1] => 'h'
 	// text[2] => 'i'
 	// string.length == strlen(text) => 12
 
-
-	// 
 	// Functions that may be usefull:
 	// isalpha('A') => true
 	// isupper('A') => true
