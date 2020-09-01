5e9b6be930d6a5be1635a55d1472d5274f1ee663
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-08-31 21:44:38 -0300

    refactor code

--- cs50/pset1/03-Skittles/skittles.c
+++ cs50/pset1/03-Skittles/skittles.c
@@ -11,8 +11,25 @@ int main(void)
 
 	do
 	{
+	  scanf("%d", &guess);
+
+	  switch(guess) {
+	  	case (guess <= 1):
+		  printf("Haha! Tenho muito mais que isso!\n");
+		  break;
+		case (guess >= 1023):
+		  printf("Ok, não são tantos assim. Tente novamente.\n");
+		  break;
+		case (guess > skittlesCount):
+	  	  printf("%s\n", "Tenho menos do que isso!\n");
+	  	  break;
+	  	case (guess < skittlesCount):
+	  	  printf("%s\n", "Tenho mais do que isso!\n");
+	  	  break;`
+	  }
 
 	} while (guess != skittlesCount);
 
 	printf("%s\n", "Parabéns, você adivinhou a quantidade exata!");
+	return 0;
 }
